7ea4c338fee894985bd70706a28f038be7c6958a
Sergey Kalinichev <mail_subscriber@mail.ru>
2015-04-04 14:56:45 +0400

    Добавлен интерфейс для снегоочистки

---
+++ Snowplug/Snowplug/Snowplug.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Snowplug.GMC;
+
+namespace Snowplug
+{
+    /// <summary>
+    /// Summary description for Snowplug
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Snowplug : System.Web.Services.WebService
+    {
+        /// <summary>
+        /// УНД командует Снегоочистке убрать снег с карты
+        /// </summary>
+        /// <param name="coordinates">Путь, чтобы проехать по всей карте</param>
+        /// <returns></returns>
+        [WebMethod]
+        public bool Clean(List<CoordinateTuple> coordinates)
+        {
+            //FIXME:
+            return true;
+        }
+
+        //FIXME: как сообщить, что операция завершена?
+    }
+}
