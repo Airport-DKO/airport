d3f29283799d9ee8d80487f01d052822ed3173c2
dimonru <wqz@ya.ru>
2015-04-08 03:26:19 +0300

    Метрологическая служба (GUI). При старте забирает время с веб-сервиса, далее каждые 5 секунд проделывает тоже самое. Можно задать коэффициент ускорения/замедления.

---
+++ Metrological Service GUI/MetrologicalService/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MetrologicalService
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(148, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Время системы";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabel.Location = new System.Drawing.Point(69, 47);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(292, 73);
+            this.timeLabel.TabIndex = 1;
+            this.timeLabel.Text = "00:00:00";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.AutoSize = true;
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateLabel.Location = new System.Drawing.Point(107, 120);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(216, 42);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "06.04.2015";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(125, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Изменить скорость";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.AutoCompleteCustomSource.AddRange(new string[] {
+            "1"});
+            this.textBoxSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSpeed.Location = new System.Drawing.Point(164, 235);
+            this.textBoxSpeed.MaxLength = 5;
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(115, 40);
+            this.textBoxSpeed.TabIndex = 4;
+            this.textBoxSpeed.Text = "1";
+            this.textBoxSpeed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSpeed_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(140, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 353);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSpeed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Метрологическая служба";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Metrological Service GUI/MetrologicalService/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Content;
+using RabbitMQ.Client.MessagePatterns;
+using RabbitMQ.Client.Events;
+
+namespace MetrologicalService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        System.Timers.Timer modTimer1 = new System.Timers.Timer(100);
+        System.Timers.Timer modTimer2 = new System.Timers.Timer(5000);
+        DateTime timer = new DateTime();
+        double ModelingSpeed = 1;
+
+        MetrologWebService.MetrologService a = new MetrologWebService.MetrologService();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            modTimer1.Elapsed += timer_Elapsed1;
+            DateTime now = DateTime.Now;
+            modTimer1.Start();
+            timer = DateTime.Now;
+            a.RefreshTick(1);
+
+            timer = a.GetCurrentTime();
+            timeLabel.Text = timer.ToString("HH:mm:ss");
+            dateLabel.Text = timer.ToString("dd.MM.yyyy");
+
+            modTimer2.Elapsed += timer_Elapsed2;
+            modTimer2.Start();
+        }
+
+        private void timer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            timer = timer.AddSeconds(1 * ModelingSpeed / 10);
+            timeLabel.Text = timer.ToString("HH:mm:ss");
+            dateLabel.Text = timer.ToString("dd.MM.yyyy");
+        }
+
+        private void timer_Elapsed2(object sender, ElapsedEventArgs e)
+        {
+            timer = a.GetCurrentTime();
+            timeLabel.Text = timer.ToString("HH:mm:ss");
+            dateLabel.Text = timer.ToString("dd.MM.yyyy");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double eee = (double)Convert.ToDouble(textBoxSpeed.Text);
+                bool valid = double.TryParse(textBoxSpeed.Text.ToString(), out eee);
+
+                if (valid && eee > 0 && eee <= 60)
+                {
+                    ModelingSpeed = eee;
+                    a.RefreshTick(1 / eee);
+                }
+                else
+                {
+                    MessageBox.Show("Неверно введено число, требуется значение больше 0 и не более 60");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверно введено число, требуется значение больше 0 и не более 60");
+            }
+        }
+
+        private void textBoxSpeed_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == ',' || e.KeyChar == '.') && !textBoxSpeed.Text.Contains(',') && (textBoxSpeed.Text.Length != 0) && (textBoxSpeed.SelectionLength == 0))
+            {
+                e.KeyChar = ',';
+            }
+            else if (!(Char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
