a25652455312a3df69c5a8ca83f9f9c63a8b8608
sprodan <sprodan8@gmail.com>
2015-04-11 22:51:00 +0300

    Визуализатор

---
+++ Vizualizer/WebApplication1/Bidlokod/MyMapObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Logic;
+
+namespace WebApplication1.Bidlokod
+{
+    public static class MyMapObject
+    {
+        public static List<Location> GetMapObjects()
+        {
+            //Из файла, Конечно тоже
+            return new List<Location> { new Location { Position = new CoordinateTuple { X = 1, Y = 2 }, MapObj = new MapObject { Type = MapObjectType.Airport, number = 1 } } };
+        }
+    }
+}
